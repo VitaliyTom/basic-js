3f78403ef5bb257847b2dbfffd843fba64d144f5
VitaliyTom <tomashevich.vitaliy88@gmail.com>
2020-03-11 14:28:33 +0300

    refactor: add package idea

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,15 +1,15 @@
 module.exports = function createDreamTeam(members) {
-	let nameTeam = '';
-	if (!Array.isArray(members)) {
-		return false;
-	}
-	for (let i = 0; i < members.length; i++) {
-		if (typeof members[i] != 'string') {
-			continue;
-		}
+    let nameTeam = '';
+    if (!Array.isArray(members)) {
+        return false;
+    }
+    for (let i = 0; i < members.length; i++) {
+        if (typeof members[i] != 'string') {
+            continue;
+        }
 		members[i] = members[i].trim();
-		nameTeam += members[i][0];
-	}
-	nameTeam = nameTeam.toLocaleUpperCase().split('').sort().join('');
-	return nameTeam;
+        nameTeam += members[i][0];
+    }
+    nameTeam = nameTeam.toLocaleUpperCase().split('').sort().join('');
+    return nameTeam;
 };
