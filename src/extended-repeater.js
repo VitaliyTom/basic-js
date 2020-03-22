623acf1a0846c0c53cc654f13c1e11cc63d60281
VitaliyTom <tomashevich.vitaliy88@gmail.com>
2020-03-22 23:51:52 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,24 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	let addition = '';
+	let repeatTimes;
+	let additionSeparator;
+	let repeatTimesAddition;
+	let separator;
+
+	if ('addition' in options) {
+		additionSeparator = 'additionSeparator' in options ? options.additionSeparator : '|';
+		repeatTimesAddition = options.additionRepeatTimes === undefined ? 1 : options.additionRepeatTimes;
+
+		addition = (additionSeparator + options.addition).repeat(repeatTimesAddition);
+		addition = addition.replace(additionSeparator, '');
+	}
+
+	if ('repeatTimes' in options) {
+		repeatTimes = options.repeatTimes === undefined ? (repeatTimes = 1) : (repeatTimes = options.repeatTimes);
+		separator = 'separator' in options ? options['separator'] : '+';
+
+		str = (separator + str + addition).repeat(repeatTimes);
+		str = str.replace(separator, '');
+	}
+	return str;
 };
-  
